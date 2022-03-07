a28186e6e9d6a7c6e7159788fc27ba7a061bc92f
KlondikeITBlogger <92996537+klondikeitblogger@users.noreply.github.com>
2022-03-07 16:01:41 +0300

    Delete js directory

--- js/prolist22.js
+++
@@ -1,9 +0,0 @@
-function scrollNav() {
-  $('nav a').click(function(){   
-    $('html, body').stop().animate({
-      scrollTop: $($(this).attr('href')).offset().top
-    }, 500);
-    return false;
-  });
-}
-scrollNav();
